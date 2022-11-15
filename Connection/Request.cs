482c76228c53966c458ef9c8322e231b87138f3a
Gustavo Silva <gustavoasc2004@gmail.com>
2022-11-14 22:25:53 -0400

    Implemented HTTP methods in Request class and added URL class.

--- Connection/Request.cs
+++ Connection/Request.cs
@@ -6,12 +6,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http.Headers;
 using System.Net;
+using Newtonsoft.Json.Linq;
 
 namespace League_of_Legends___Command_Line_Interface.Connection
 {
 	public class Request
 	{
-		private const string _url = "https://127.0.0.1:";
+		private string _url = $"https://127.0.0.1:{Setup.Port}";
 		private HttpClient _httpClient;
 		private HttpClientHandler _httpClientHandler;
 
@@ -23,6 +24,8 @@ namespace League_of_Legends___Command_Line_Interface.Connection
 			};
 
 			_httpClientHandler.ServerCertificateCustomValidationCallback = (response, cert, chain, errors) => true;
+
+
 		}
 
 		private async Task CreateClient()
@@ -34,6 +37,27 @@ namespace League_of_Legends___Command_Line_Interface.Connection
 				client.DefaultRequestHeaders.Add("Authorization", password);
 				client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 			}
+		}
+
+		public async Task<HttpResponseMessage> Get(string endpoint)
+		{
+			_url += "/" + endpoint;
+			HttpResponseMessage response = await _httpClient.GetAsync(endpoint);
+		}
+
+		public async Task<HttpResponseMessage> Post()
+		{
+
+		}
+
+		public async Task<HttpResponseMessage> Put()
+		{
+
+		}
+
+		public async Task<JObject> GetContent()
+		{
+
 		}
 	}
 }
